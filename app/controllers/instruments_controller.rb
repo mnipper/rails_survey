bc309b084dcc9dd168694edb7aade9035d13f302
Michael Nipper <mjn4406@gmail.com>
2013-10-24 15:27:23 -0400

    Allow instrument edit

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -26,7 +26,7 @@ class InstrumentsController < ApplicationController
 
   def update
     @instrument = Instrument.find(params[:id])
-    if @instrument.update_attributes(params[:instrument])
+    if @instrument.update_attributes(instrument_params)
       redirect_to @instrument, notice: "Successfully updated instrument."
     else
       render :edit
