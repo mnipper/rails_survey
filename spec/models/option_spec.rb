a38a633d7698c658b10b293937e36187bf462221
Michael Nipper <mjn4406@gmail.com>
2013-11-06 13:10:11 -0500

    Option method specs

--- spec/models/option_spec.rb
+++ spec/models/option_spec.rb
@@ -1,6 +1,8 @@
 require "spec_helper"
 
 describe Option do
+  it { should respond_to(:question) }
+
   it "should return the text when to_s" do
     option = Option.create(text: 'a')
     option.to_s.should == 'a'
