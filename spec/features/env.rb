317c14e21474dc8ed8f476ef3b6f4d95905f17fb
Michael Nipper <mjn4406@gmail.com>
2013-11-07 17:08:46 -0500

    Bump up selenium sleep time

--- spec/features/env.rb
+++
@@ -1,5 +0,0 @@
-Capybara.register_driver :selenium do |app|
-  http_client = Selenium::WebDriver::Remote::Http::Default.new
-  http_client.timeout = 300
-  Capybara::Selenium::Driver.new(app, :browser => :firefox, :http_client => http_client)
-end
