bf5d431b649d6083fe81c287f436058ea5300413
Michael Nipper <mjn4406@gmail.com>
2013-11-07 14:08:53 -0500

    Add remove question spec

--- spec/features/instrument_creation_spec.rb
+++ spec/features/instrument_creation_spec.rb
@@ -4,15 +4,21 @@ feature "Instrument Creation" do
   before :each do
     @user = FactoryGirl.build(:user)
     @user.save!
-  end
-
-  scenario "User adds a new question", js: true do
     visit '/users/sign_in'
     fill_in 'user_email', :with => @user.email
     fill_in 'user_password', :with => @user.password
     click_button 'Sign in'
     visit "/instruments/new"
+  end
+
+  scenario "user adds a new question", js: true do
     click_link "Add Question"
     expect(page).to have_text("Question type")
+  end
+
+  scenario "user removes a question", js: true do
+    click_link "Add Question"
+    click_link "Remove Question"
+    expect(page).to_not have_text("Question type")
   end
 end
