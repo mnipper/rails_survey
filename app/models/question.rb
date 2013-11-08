a51f77dfdd30d4fc8b38910534172fd39cb94364
Michael Nipper <mjn4406@gmail.com>
2013-11-08 09:53:02 -0500

    Clean up response count

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,8 +3,4 @@ class Question < ActiveRecord::Base
   has_many :responses
   has_many :options
   accepts_nested_attributes_for :options, allow_destroy: true
-
-  def response_count
-    self.responses.count
-  end
 end
