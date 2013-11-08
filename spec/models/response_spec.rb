d0a5685fbaa4e432d33c32cd9f4e10e35d7f240e
Michael Nipper <mjn4406@gmail.com>
2013-11-08 09:06:16 -0500

    Speed up tests by scoping before blocks better

--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -1,26 +1,28 @@
 require "spec_helper"
 
 describe Response do
-  before :each do
-    @response = build(:response)
-    @survey = create(:survey)
-  end
-
   it { should respond_to(:question) }
   it { should respond_to(:survey) }
 
-  it "should return the survey by UUID" do
-    @response.survey_uuid = @survey.uuid
-    @response.survey.should == @survey
-  end
+  describe "When there is a valid response" do
+    before :each do
+      @response = build(:response)
+    end
 
-  it "should return text for to_s if no options" do
-    @response.to_s.should == @response.text
-  end
+    it "should return the survey by UUID" do
+      @survey = create(:survey)
+      @response.survey_uuid = @survey.uuid
+      @response.survey.should == @survey
+    end
 
-  it "should return option text for to_s if options" do
-    question = build(:question)
-    question.options << build(:option)
-    @response.to_s.should == question.options.first.to_s
+    it "should return text for to_s if no options" do
+      @response.to_s.should == @response.text
+    end
+
+    it "should return option text for to_s if options" do
+      question = build(:question)
+      question.options << build(:option)
+      @response.to_s.should == question.options.first.to_s
+    end
   end
 end
--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -1,16 +1,13 @@
 require "spec_helper"
 
 describe Survey do
-  before :each do
-    @survey = build(:survey) 
-  end
-
   it { should respond_to(:instrument) }
   it { should respond_to(:device) }
   it { should respond_to(:responses) }
   it { should respond_to(:percent_complete) }
 
   it "should return the correct responses" do
+    @survey = build(:survey) 
     response = Response.create(survey_uuid: @survey.uuid)
     @survey.responses.should == [response] 
   end
