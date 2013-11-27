d8a2038c1049e07d0f02b4eed8a37efff8ded16e
Michael Nipper <mjn4406@gmail.com>
2013-11-27 16:02:35 -0500

    Add some navigation integration tests

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -2,8 +2,7 @@ require "spec_helper"
 
 feature "Navigation" do
   before :each do
-    @user = FactoryGirl.build(:user)
-    @user.save!
+    @user = create(:user)
     visit '/users/sign_in'
     fill_in 'user_email', :with => @user.email
     fill_in 'user_password', :with => @user.password
@@ -17,6 +16,33 @@ feature "Navigation" do
         expect(page).to have_text("View Instruments")
         expect(page).to have_text("New Instrument")
       end
+    end
+  end
+
+  scenario "device sidebar click shows devices", js: true do
+    within('#sidebar') do
+      click_link "Devices"
+    end
+    within('section.widget') do
+      expect(page).to have_text("Devices")
+    end
+  end
+
+  scenario "survey sidebar click shows surveys", js: true do
+    within('#sidebar') do
+      click_link "Survey Results"
+    end
+    within('section.widget') do
+      expect(page).to have_text("Survey Results")
+    end
+  end
+
+  scenario "notifications sidebar click shows notifications", js: true do
+    within('#sidebar') do
+      click_link "Notifications"
+    end
+    within('section.widget') do
+      expect(page).to have_text("Notifications")
     end
   end
 end
