d9df0ab8e92afe166fd4b8ada092a7b94dcbdd4b
Michael Nipper <mjn4406@gmail.com>
2013-12-02 10:57:48 -0500

    Get rid of un-used method

--- app/models/question.rb
+++ app/models/question.rb
@@ -17,9 +17,4 @@ class Question < ActiveRecord::Base
   has_many :responses
   has_many :options
   accepts_nested_attributes_for :options, allow_destroy: true
-
-  def self.ids_for_single_and_multiple_select_questions(question_type)
-    Question.select(:id).where(:question_type => question_type)
-  end
-
 end
