5380312c47747d78d33db0d0f8ce40c17b2851f4
Michael Nipper <mjn4406@gmail.com>
2013-12-04 12:16:15 -0500

    Instrument translation spec

--- spec/features/instrument_translation_spec.rb
+++ spec/features/instrument_translation_spec.rb
@@ -18,13 +18,21 @@ feature "Instrument Translation", js: true do
 
   scenario "clicking new translation shows new translation page" do
     click_link "New Translation"
-    expect(page).to have_text("Translation for Test Instrument")
+    click_button 'Create Instrument translation'
+    expect(page).to have_text("Successfully created instrument translation")
   end
 
   scenario "editing the title changes the title" do
     click_link "New Translation"
     fill_in 'instrument_translation_title', :with => 'New Title'
     click_button 'Create Instrument translation'
-    expect(page).to have_text("Translation for Test Instrument")
+    expect(page).to have_text("New Title")
+  end
+
+  scenario "adding a translation saves" do
+    click_link "New Translation"
+    find(:css, "textarea[id^='question_translations']").set("Translated Question")
+    click_button 'Create Instrument translation'
+    expect(page).to have_text("Translated Question")
   end
 end
