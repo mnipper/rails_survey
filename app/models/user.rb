c276203eef4e9f279332b3b2ce90af94675aa2f0
Michael Nipper <mjn4406@gmail.com>
2013-12-09 13:16:21 -0500

    Remove registerable module from devise and update password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,8 +21,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable,
+          :trackable, :validatable
 
   before_save :ensure_authentication_token
 
