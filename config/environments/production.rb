818e5e968968ab8a88202ae988dd05413b8896ee
Michael Nipper <mjn4406@gmail.com>
2013-12-16 16:17:47 -0500

    Precompile images in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,7 @@ RailsSurvey::Application.configure do
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
+  config.assets.precompile += %w( '.woff', '.eot', '.svg', '.ttf' )
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
