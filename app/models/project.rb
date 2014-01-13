25cc27d2a4de03595ec2a476082eece978285960
Leonard Ng'eno <dranoel.bravo@gmail.com>
2014-01-13 13:04:15 -0500

    format projects view

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,7 @@
 class Project < ActiveRecord::Base
+
+  attr_accessible :name, :description
+  validates :name, presence: true, allow_blank: false
+  validates :description, presence: true, allow_blank: true
+
 end
