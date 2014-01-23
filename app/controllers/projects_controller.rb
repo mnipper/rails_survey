55d2d1fb763ebdf55e4bc0b1d8e1dd8330238b4b
Leonard Ng'eno <dranoel.bravo@gmail.com>
2014-01-23 11:11:36 -0500

    associate a project with a user

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,56 +1,44 @@
 class ProjectsController < ApplicationController
 
   def index
-    @projects = Project.all
+    @projects = current_user.projects
   end
 
   def show
-    @project = Project.find(params[:id])
+    @project = current_user.projects.find(params[:id])
     set_current_project @project
   end
 
   def new
-    @project = Project.new
+    @project = current_user.projects.new
   end
 
   def edit
-    @project = Project.find(params[:id])
+    @project = current_user.projects.find(params[:id])
   end
 
   def create
     @project = Project.new(project_params)
-
-    respond_to do |format|
-      if @project.save
-        format.html { redirect_to @project, notice: 'Project was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @project }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+    if @project.save && @project.user_projects.create(:user_id => current_user.id, :project_id => @project.id)
+      redirect_to @project, notice: 'Project was successfully created.'
+    else
+      render :new
     end
   end
 
   def update
-    @project = Project.find(params[:id])
-    respond_to do |format|
-      if @project.update(project_params)
-        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+    @project = current_user.projects.find(params[:id])
+    if @project.update(project_params)
+      redirect_to @project, notice: 'Project was successfully updated.'
+    else
+      render :edit
     end
   end
 
   def destroy
-    @project = Project.find(params[:id])
+    @project = current_user.projects.find(params[:id])
     @project.destroy
-    respond_to do |format|
-      format.html { redirect_to projects_url }
-      format.json { head :no_content }
-    end
+    redirect_to projects_url
   end
 
   private
