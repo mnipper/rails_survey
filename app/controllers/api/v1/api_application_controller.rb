4656d150c4ef750deacda2b086c8c34082ebca1e
Michael Nipper <mjn4406@gmail.com>
2014-01-27 16:11:43 -0500

    Allow a currently logged in user to interact with api without key

--- app/controllers/api/v1/api_application_controller.rb
+++ app/controllers/api/v1/api_application_controller.rb
@@ -18,8 +18,10 @@ module Api
 
       private
         def restrict_access
-          api_key = ApiKey.find_by_access_token(params[:access_token])
-          head :unauthorized unless api_key
+          unless current_user
+            api_key = ApiKey.find_by_access_token(params[:access_token])
+            head :unauthorized unless api_key
+          end
         end
     end
   end
