1de85604308379e6dac204d9f5261358efb5b2c8
Michael Nipper <mjn4406@gmail.com>
2014-01-28 11:46:35 -0500

    Fix navigation feature spec

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -3,10 +3,13 @@ require "spec_helper"
 feature "Navigation" do
   before :each do
     @user = create(:user)
+    @project = create(:project)
+    @user_project = UserProject.create!(user_id: @user.id, project_id: @project.id)
     visit '/users/sign_in'
     fill_in 'user_email', :with => @user.email
     fill_in 'user_password', :with => @user.password
     click_button 'Sign in'
+    visit "/projects/#{@project.id}"
   end
 
   scenario "user collapses instruments sidebar", js: true do
