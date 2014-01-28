60bf7828b41ecf39f32a502a094d809ec823756a
Michael Nipper <mjn4406@gmail.com>
2014-01-27 20:00:47 -0500

    Add option to reject android api calls if below minimum version number

--- app/controllers/api/v1/api_application_controller.rb
+++ app/controllers/api/v1/api_application_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class ApiApplicationController < ApplicationController
       before_filter :restrict_access
-
+      before_filter :check_version_code
       skip_before_filter :authenticate_user!
       skip_before_filter :authenticate_user_from_token!
       after_filter :set_csrf_cookie_for_ng
@@ -22,6 +22,10 @@ module Api
             api_key = ApiKey.find_by_access_token(params[:access_token])
             head :unauthorized unless api_key
           end
+        end
+
+        def check_version_code
+          head :upgrade_required unless params[:version_code].to_i >= Settings.minimum_android_version_code
         end
     end
   end
