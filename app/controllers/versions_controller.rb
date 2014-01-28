2eec6565d7fbaad676c41e9b9f57343d4d38e904
Michael Nipper <mjn4406@gmail.com>
2014-01-28 16:57:57 -0500

    Fix instrument project path bug

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,16 +1,17 @@
 class VersionsController < ApplicationController
   def index
     @project = Project.find(params[:project_id])
-    @instrument = Instrument.find(params[:instrument_id])
+    @instrument = @project.instruments.find(params[:instrument_id])
   end
 
   def show
-    @instrument = Instrument.find(params[:instrument_id])
+    @project = Project.find(params[:project_id])
+    @instrument = @project.instruments.find(params[:instrument_id])
     @version = @instrument.versions[params[:id].to_i]
     if @version
       @instrument_version = @version.reify
     else
-      redirect_to @instrument
+      redirect_to [@project, @instrument]
     end
   end
 end
