892a3fbef5793d057ffe21da418be19a37ed461c
Michael Nipper <mjn4406@gmail.com>
2014-01-31 17:02:30 -0500

    Fix redirect to current instrument version bug

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,11 +1,10 @@
 class VersionsController < ApplicationController
   def index
-    @project = Project.find(params[:project_id])
-    @instrument = Instrument.find(params[:instrument_id])
+    @instrument = current_project.instruments.find(params[:instrument_id])
   end
 
   def show
-    @instrument = current_project.find(params[:instrument_id])
+    @instrument = current_project.instruments.find(params[:instrument_id])
     @version = @instrument.versions[params[:id].to_i]
     if @version
       @instrument_version = @version.reify
