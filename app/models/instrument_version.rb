8b9a44234cdb70a143fb07083410ec4406675b3c
Michael Nipper <mjn4406@gmail.com>
2014-02-10 15:01:57 -0500

    Include deleted questions and options for instrument version

--- app/models/instrument_version.rb
+++ app/models/instrument_version.rb
@@ -27,7 +27,7 @@ class InstrumentVersion
   def questions
     return @instrument.questions unless @version
     questions = []
-    @version.reify.questions.each do |question|
+    @version.reify.questions.with_deleted.each do |question|
       versioned_question = versioned(question)
       if versioned_question
         questions << versioned_question
@@ -47,7 +47,7 @@ class InstrumentVersion
   def options_for_question(question)
     return question.options unless @version
     options = []
-    question.options.each do |option|
+    question.options.with_deleted.each do |option|
       options << versioned(option) if versioned(option)
     end
     options
