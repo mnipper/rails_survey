d980a8428f7d0c3e84d5793644ae1a288dfc0470
Michael Nipper <mjn4406@gmail.com>
2014-02-18 16:26:00 -0500

    Scope instrument projects to current project

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,47 +1,40 @@
 class InstrumentsController < ApplicationController
   def index
-    @project = Project.find(params[:project_id])
-    @instruments = @project.instruments
+    @instruments = current_project.instruments
   end
 
   def show
-    @project = Project.find(params[:project_id])
-    @instrument = @project.instruments.find(params[:id])
+    @instrument = current_project.instruments.find(params[:id])
   end
 
   def new
-    @project = Project.find(params[:project_id])
-    @instrument = @project.instruments.new
+    @instrument = current_project.instruments.new
   end
 
   def create
-    @project = Project.find(params[:project_id])
-    @instrument = @project.instruments.new(params[:instrument])
+    @instrument = current_project.instruments.new(params[:instrument])
     if @instrument.save
-      redirect_to project_instrument_path(@project, @instrument), notice: "Successfully created instrument."
+      redirect_to project_instrument_path(current_project, @instrument), notice: "Successfully created instrument."
     else
       render :new
     end
   end
 
   def edit
-    @project = Project.find(params[:project_id])
-    @instrument = @project.instruments.find(params[:id])
+    @instrument = current_project.instruments.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:project_id])
-    @instrument = @project.instruments.find(params[:id])
+    @instrument = current_project.instruments.find(params[:id])
     if @instrument.update_attributes(params[:instrument])
-      redirect_to project_instrument_path(@project, @instrument), notice: "Successfully updated instrument."
+      redirect_to project_instrument_path(current_project, @instrument), notice: "Successfully updated instrument."
     else
       render :edit
     end
   end
 
   def destroy
-    @project = Project.find(params[:project_id])
-    @instrument = @project.instruments.find(params[:id])
+    @instrument = current_project.instruments.find(params[:id])
     @instrument.destroy
     redirect_to project_instruments_url, notice: "Successfully destroyed instrument."
   end
