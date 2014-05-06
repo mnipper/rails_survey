0f41d13b31572a49bd0473dc55ea18775c1caec8
Leonard Ng'eno <leonard@leonard-VirtualBox.(none)>
2014-05-06 16:13:11 -0400

    add capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,63 @@
+# config valid only for Capistrano 3.1
+lock '3.2.1'
+
+
+set :application, 'rails_survey' 
+set :repository, 'git@github.com:mnipper/rails_survey.git' 
+set :scm, :git
+set :branch, "master" 
+set :user, "dmtg"
+set :use_sudo, false
+set :rails_env, "production"
+set :deploy_via, :copy
+set :ssh_options, { :forward_agent => true, :port => 2222 }
+set :keep_releases, 5
+# default_run_options[:pty] = true
+server "wci-chpir.duke.edu", roles: [:app, :web, :db], user: "dmtg", port: 2222, :primary => true 
+
+
+# Default value for :format is :pretty
+
+
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
