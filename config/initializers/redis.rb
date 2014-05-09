195cbf845de7c3d8a5dd7a1b1b4594e3144fc0f6
Leonard Ng'eno <dranoel.bravo@gmail.com>
2014-05-09 16:13:43 -0400

    comment out heroku redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
-if Rails.env.production?
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :username => uri.user)
-else
+#if Rails.env.production?
+#  uri = URI.parse(ENV["REDISTOGO_URL"])
+#  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :username => uri.user)
+#else
   $redis = Redis.new(:host => 'localhost', :port => 6379)
-end
+#end
