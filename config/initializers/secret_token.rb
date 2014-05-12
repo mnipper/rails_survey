d5e73a32e9e29ae84312ac01e914d560ea6ebefc
Leonard Ngeno <dranoel.bravo@gmail.com>
2014-05-12 16:27:34 -0400

    Symlink secret key text

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -12,5 +12,5 @@
 RailsSurvey::Application.config.secret_key_base = if Rails.env.development? or Rails.env.test?
   '7936e9671547a57d49c0e998bd38cf054a0b9542e030d0cc8c3cd8907bbc57ee01aefb113e6c63e1446c7d1ca541135b466208400a56e3d1515b21103bd58b64'
 else
-  File.read(Settings.secret_key_path)
+  File.read(File.join(Rails.root, 'config', 'secret_token.txt'))
 end
