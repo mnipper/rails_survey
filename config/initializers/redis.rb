4d2ccad9afb1683fd1ee3f218e3f92ef575eb36c
Leonard Ngeno <dranoel.bravo@gmail.com>
2014-05-12 16:46:54 -0400

    remove heroku redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,1 @@
-#if Rails.env.production?
-#  uri = URI.parse(ENV["REDISTOGO_URL"])
-#  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :username => uri.user)
-#else
-  $redis = Redis.new(:host => 'localhost', :port => 6379)
-#end
+$redis = Redis.new(:host => 'localhost', :port => 6379)
