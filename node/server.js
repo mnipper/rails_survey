9cef34250c89e80f066ed2631229ba4b6d89ee46
Leonard Ng'eno <dranoel.bravo@gmail.com>
2014-05-16 11:18:44 -0400

    run node on ssl

--- node/server.js
+++ node/server.js
@@ -3,17 +3,15 @@ process.title = 'realtime-app';
 var sockjs  = require('sockjs');
 var express = require('express');
 var app = express();
-var http = require('http');
-//var https = require('https');
-//var fs = require('fs');
-//var options = {
-//	    key:    fs.readFileSync('wci-chpir_duke_edu-private.key'),
-//	    cert:   fs.readFileSync('wci-chpir_duke_edu.cer')
-//	};
-//TODO Extract above certs
-//var server = https.createServer(options, app);
-//var server = https.createServer(app);
-var server = http.createServer(app);
+var https = require('https');
+var fs = require('fs');
+var options = {
+	    key:    fs.readFileSync('wci-chpir_duke_edu-private.key'),
+	    cert:   fs.readFileSync('wci-chpir_duke_edu.cer')
+	};
+TODO Extract above certs
+var server = https.createServer(options, app);
+var server = https.createServer(app);
 var io = require('socket.io').listen(server);
 var redis = require("redis");
 server.listen(8080);
