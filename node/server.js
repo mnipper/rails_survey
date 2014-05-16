4a6ac8af276b09ad9b819d3bd507e45afe2f7fd7
Leonard Ng'eno <dranoel.bravo@gmail.com>
2014-05-16 15:00:35 -0400

    add dirname constant

--- node/server.js
+++ node/server.js
@@ -6,8 +6,8 @@ var app = express();
 var https = require('https');
 var fs = require('fs');
 var options = {
-	    key:    fs.readFileSync(__dirname + '../../../../../../etc/httpd/conf/ssl.key/wci-chpir_duke_edu-private.key'),
-	    cert:   fs.readFileSync(__dirname + '../../../../../../etc/httpd/conf/ssl.crt/wci-chpir_duke_edu.cer')
+	    key:    fs.readFileSync('../../../../../../etc/httpd/conf/ssl.key/wci-chpir_duke_edu-private.key'),
+	    cert:   fs.readFileSync('../../../../../../etc/httpd/conf/ssl.crt/wci-chpir_duke_edu.cer')
 	};
 var server = https.createServer(options, app);
 var server = https.createServer(app);
