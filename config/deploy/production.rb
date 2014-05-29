92b356edd1e12baab132c96a69964249ef2627cc
Leonard Ngeno <dranoel.bravo@gmail.com>
2014-05-29 16:54:42 -0400

    use different server for wci-chpir database

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,8 +2,8 @@
 set :stage, :production
 set :branch, 'master'
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
-#set :server_name, 'wci-chpir.duke.edu'
-server 'wci-chpir.duke.edu', user: 'dmtg', roles: %w{web app db}, primary: true 
+server 'chpir-survey-01.oit.duke.edu', user: 'dmtg', roles: %w{db}, primary: true 
+server 'wci-chpir.duke.edu', user: 'dmtg', roles: %w{web app}, primary: true 
 server 'adaptlab.vm.duke.edu', user: 'rover', roles: %w{web app db}, primary: true 
 set :deploy_to, '/var/www/rails_survey'
 set :rails_env, :production
