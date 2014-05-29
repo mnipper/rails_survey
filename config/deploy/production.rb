d208fd2b4662846964a800b3b84c906a35e0cfef
Leonard Ngeno <dranoel.bravo@gmail.com>
2014-05-29 16:47:25 -0400

    do not use set server_name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@
 set :stage, :production
 set :branch, 'master'
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
-set :server_name, 'wci-chpir.duke.edu'
+#set :server_name, 'wci-chpir.duke.edu'
 server 'wci-chpir.duke.edu', user: 'dmtg', roles: %w{web app db}, primary: true 
 server 'adaptlab.vm.duke.edu', user: 'rover', roles: %w{web app db}, primary: true 
 set :deploy_to, '/var/www/rails_survey'
