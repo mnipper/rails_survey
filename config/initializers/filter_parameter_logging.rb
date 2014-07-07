894af7f2ca68eec89d681fe3fc7fb8d2d6945998
Michael Nipper <mjn4406@gmail.com>
2014-07-07 17:37:09 -0400

    Filter params in production

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :text]
+if Rails.env.production?
+  Rails.application.config.filter_parameters += [:password, :text, :access_token]
+end
