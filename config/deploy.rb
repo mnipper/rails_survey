5528f7f6c401bb599bf20fc1a013c734159f14b3
Leonard Ng'eno <dranoel.bravo@gmail.com>
2014-07-18 16:15:35 -0400

    set linked dirs & sidekiq processes

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,12 +11,13 @@ set :pty, false
 set :format, :pretty
 set :keep_releases, 5
 set :linked_files, %w{config/database.yml config/secret_token.txt config/local_env.yml}
-set :linked_dirs, fetch(:linked_dirs).push("app/files" "bin" "log" "tmp/pids" "tmp/cache" "tmp/sockets" "vendor/bundle" "public/system")
+set :linked_dirs, %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle)
+set :linked_dirs, fetch(:linked_dirs) + %w{public/system public/files}
 set :branch, 'master'
 set :sidekiq_pid, File.join(shared_path, 'tmp', 'pids', 'sidekiq.pid')
 set :sidekiq_log, File.join(shared_path, 'log', 'sidekiq.log')
 set :sidekiq_concurrency, 25
-set :sidekiq_processes, 4
+set :sidekiq_processes, 1
 
 namespace :deploy do
  
