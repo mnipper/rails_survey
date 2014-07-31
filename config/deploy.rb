681686216adc326d177a3c9e05105dea0c144b04
Leonard Ng'eno <dranoel.bravo@gmail.com>
2014-07-31 14:23:38 -0400

    restart monit after deploy:restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,7 +47,7 @@ namespace :deploy do
   after :finishing, 'deploy:cleanup'
   after 'deploy:publishing', 'deploy:restart'
   after "deploy:updated", "deploy:npm_install"
-  after 'deploy:setup', 'monit:restart'
+  after 'deploy:restart', 'monit:restart'
 end
 
 namespace :monit do
