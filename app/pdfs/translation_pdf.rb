80536d79c3b2945d0d56a06165241a87338abb99
Michael Nipper <mjn4406@gmail.com>
2014-11-05 15:09:40 -0500

    Pull out some magic numbers

--- app/pdfs/translation_pdf.rb
+++ app/pdfs/translation_pdf.rb
@@ -2,20 +2,25 @@ class TranslationPdf < Prawn::Document
   def initialize(instrument_translation)
     super()
     @instrument_translation = instrument_translation
-    @instrument = instrument_translation.instrument
+    @instrument             = instrument_translation.instrument
+    @language               = @instrument_translation.language
+    @version                = @instrument.current_version_number
+
     header
     content
   end
 
   def display_name
-    "#{@instrument.title}_#{@instrument.current_version_number}_#{@instrument_translation.language}.pdf"
+    "#{@instrument.title}_#{@version}_#{@language}.pdf"
   end
 
   private
     def header
-      text "#{@instrument_translation.title} v#{@instrument.current_version_number}", size: 20, style: :bold
-      text "#{@instrument_translation.language}"
-      move_down 25 
+      after_title_margin = 15
+
+      text "#{@instrument_translation.title} v#{@version}", size: 20, style: :bold
+      text "#{@language}"
+      move_down after_title_margin
     end
    
     def content
@@ -25,12 +30,14 @@ class TranslationPdf < Prawn::Document
     end
 
     def format_question(question)
+      instruction_question_margin = 10
+
       number_question(question)
 
-      if question.has_translation_for?(@instrument_translation.language)
+      if question.has_translation_for?(@language)
         text question.instructions, style: :italic
-        move_down 10
-        text Sanitize.fragment(question.translated_for(@instrument_translation.language, :text))
+        move_down instruction_question_margin 
+        text Sanitize.fragment(question.translated_for(@language, :text))
         draw_options(question) if question.has_options?
       end
 
@@ -38,21 +45,27 @@ class TranslationPdf < Prawn::Document
     end 
 
     def number_question(question)
+      left_margin = 30
+
       text "#{question.number_in_instrument}.)", size: 18, style: :bold
-      draw_text question.question_type, at: [30, cursor + 15], size: 10, style: :bold
-      draw_text question.question_identifier, at: [30, cursor + 5], size: 10, style: :bold
+      draw_text question.question_type,       at: [left_margin, cursor + 15], size: 10, style: :bold
+      draw_text question.question_identifier, at: [left_margin, cursor + 5],  size: 10, style: :bold
     end
 
     def draw_options(question)
+      left_margin = 10
+      circle_size = 5
+      after_option_margin = 15
+
       question.options.each do |option|
-        stroke_circle [10, cursor - 5], 5
-        draw_text option.translated_for(@instrument_translation.language, :text), at: [20, cursor - 10]
-        move_down 15
+        stroke_circle [left_margin, cursor - 5], circle_size
+        draw_text option.translated_for(@language, :text), at: [left_margin + 10, cursor - 10]
+        move_down after_option_margin 
       end
 
       if question.has_other?
-        stroke_circle [10, cursor - 5], 5
-        draw_text "____________________________________________", at: [20, cursor - 10]
+        stroke_circle [left_margin, cursor - 5], circle_size 
+        draw_text "____________________________________________", at: [left_margin + 10, cursor - 10]
       end
     end
 
