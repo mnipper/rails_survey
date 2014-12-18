4a172a5a6c7df62121117559e0d36984969c8989
Michael Nipper <mjn4406@gmail.com>
2014-12-18 15:56:42 -0500

    Format for inline text for skip pattern errors

--- app/pdfs/translation_pdf.rb
+++ app/pdfs/translation_pdf.rb
@@ -84,7 +84,7 @@ class TranslationPdf < Prawn::Document
         if option.next_question?
           next_question = Question.find_by_question_identifier(option.next_question)
           if next_question.nil?
-            text "#{option.translated_for(@language, :text)} (<color rgb='ff0000'>Error Locating Question #{option.next_question} for skip pattern!</color>)"
+            text "#{option.translated_for(@language, :text)} (<color rgb='ff0000'>Error Locating Question #{option.next_question} for skip pattern!</color>)", :inline_format => true
           else
             text "#{option.translated_for(@language, :text)} (#{skip_to} ##{next_question.number_in_instrument})"
           end
