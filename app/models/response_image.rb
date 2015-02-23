b8cf4b94c3393abf286568ceb08d8594678c4177
Leonard Ng'eno <dranoel.bravo@gmail.com>
2015-02-23 13:13:03 -0500

    check if attachment exists

--- app/models/response_image.rb
+++ app/models/response_image.rb
@@ -47,16 +47,19 @@ class ResponseImage < ActiveRecord::Base
     zipped_file = File.open(zipped_file, "a+")
     Zip::OutputStream.open(zipped_file.path) do |zipfile|
       all.each do |response_image|
-        title = response_image.versioned_question(response_image.response.question_identifier).question_identifier + '-' + response_image.response.id.to_s + '-' + response_image.picture_file_name
-        zipfile.put_next_entry("#{name}/#{title}")
-        photos_root = File.join('files').to_s
-        photo_path = response_image.picture.url.split('?')
-        photo_path = photo_path[0]
-        path_arr = photo_path.split('/')
-        path_arr.insert(3, 'original')
-        photo_abs_url = photos_root + path_arr.join("/")
-        photo_data = open(photo_abs_url)
-        zipfile.print IO.read(photo_data)
+        if response_image.picture.exists?
+          title = response_image.versioned_question(response_image.response.question_identifier).question_identifier + '-' + 
+            response_image.response.id.to_s + '-' + response_image.picture_file_name
+          zipfile.put_next_entry("#{name}/#{title}")
+          photos_root = File.join('files').to_s
+          photo_path = response_image.picture.url.split('?')
+          photo_path = photo_path[0]
+          path_arr = photo_path.split('/')
+          path_arr.insert(3, 'original')
+          photo_abs_url = photos_root + path_arr.join("/")
+          photo_data = open(photo_abs_url)
+          zipfile.print IO.read(photo_data)
+        end
       end
     end
     zipped_file.close
