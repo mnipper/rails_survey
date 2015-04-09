433184acdf06d8a4f9a89e658b5594970d5067f2
Leonard Ng'eno <dranoel.bravo@gmail.com>
2015-04-09 14:31:13 -0400

    add option when grid label is added

--- app/models/grid_label.rb
+++ app/models/grid_label.rb
@@ -14,5 +14,18 @@ class GridLabel < ActiveRecord::Base
   attr_accessible :label, :grid_id, :option_id
   belongs_to :grid
   belongs_to :option
+  after_create :create_options
   validates :label, presence: true, allow_blank: false
+  
+  def create_options
+    if option_id.blank? && !grid.questions.blank?
+      grid.questions.each do |question|
+        option = question.options.new(:text => label, :number_in_question => question.options.count + 1)
+        if option.save
+          option_id = option.id 
+        end
+      end
+    end
+  end
+  
 end
