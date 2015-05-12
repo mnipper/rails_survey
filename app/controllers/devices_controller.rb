01ce7258b71de4a519a690eb2c30369c4ee7d5da
Leonard Ng'eno <dranoel.bravo@gmail.com>
2015-05-12 17:59:47 -0400

    use aggregate function in group by

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,7 +1,8 @@
 class DevicesController < ApplicationController
   
   def index
-    with_sync_entries = current_project.devices.group(:identifier).joins(:device_sync_entries).order('device_sync_entries.updated_at DESC')
+    with_sync_entries = current_project.devices.select('devices.*, MAX(device_sync_entries.updated_at) AS most_recent')
+      .joins(:device_sync_entries).order('most_recent DESC').group('devices.id')
     without_sync_entries = current_project.devices.includes(:device_sync_entries).where('device_sync_entries.id is null') 
     @devices = with_sync_entries + without_sync_entries
   end
